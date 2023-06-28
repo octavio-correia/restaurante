a64510a22d826cfb8bb9b43dee1780b345a44ca9
Octávio Correia <octavio.correia2017@gmail.com>
2023-06-27 22:54:09 -0300

    Corpo do site
    Melhorias no corpo do site

---
+++ site/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var texto = document.querySelector('.texto');
+    texto.classList.remove('hidden');
+    texto.style.top = '50%';
+    texto.style.opacity = '1';
+  });
