4380f9cab3c066acac7b4dddbd834b34e534c680
Octávio Correia <octavio.correia2017@gmail.com>
2023-06-28 16:36:56 -0300

    Ajustes no JS
    Consegui fazer o texto aparecer lentamente na tela. Tava complicado, mas é por que eu nao estava abrindo com o Live Server

--- site/script.js
+++ site/script.js
@@ -1,6 +1,4 @@
 window.addEventListener('DOMContentLoaded', function() {
     var texto = document.querySelector('.texto');
-    texto.classList.remove('hidden');
-    texto.style.top = '50%';
     texto.style.opacity = '1';
   });
