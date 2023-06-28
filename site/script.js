f3619fdce9aecfc9f65d08742974e556469aeae8
Octávio Correia <octavio.correia2017@gmail.com>
2023-06-28 17:06:28 -0300

    Ajustes JS
    Fiz as palavras no cabeçalho crescerem e diminuírem quando o mouse entra e sai

--- site/script.js
+++ site/script.js
@@ -1,4 +1,18 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var texto = document.querySelector('.texto');
-    texto.style.opacity = '1';
-  });
+window.addEventListener('DOMContentLoaded', function () {
+  var texto = document.querySelector('.texto');
+  texto.style.opacity = '1';
+});
+
+var grow = document.getElementsByClassName("menusTopo")
+
+function cresce(elemento) {
+  var tamanhoAtual = window.getComputedStyle(elemento).getPropertyValue("font-size");
+  var tamanhoNovo = parseFloat(tamanhoAtual) + 3;
+  elemento.style.fontSize = tamanhoNovo + "px";
+}
+
+function diminui(elemento) {
+  var tamanhoAtual = window.getComputedStyle(elemento).getPropertyValue("font-size");
+  var tamanhoNovo = parseFloat(tamanhoAtual) - 3;
+  elemento.style.fontSize = tamanhoNovo + "px";
+}
