d7fa67b43a7c843f7272bd83184a72f6a5f343fe
Octávio Correia <octavio.correia2017@gmail.com>
2023-06-29 17:58:29 -0300

    Updates no código

--- site/script.js
+++ site/script.js
@@ -2,17 +2,3 @@ window.addEventListener('DOMContentLoaded', function () {
   var texto = document.querySelector('.texto');
   texto.style.opacity = '1';
 });
-
-var grow = document.getElementsByClassName("menusTopo")
-
-function cresce(elemento) {
-  var tamanhoAtual = window.getComputedStyle(elemento).getPropertyValue("font-size");
-  var tamanhoNovo = parseFloat(tamanhoAtual) + 3;
-  elemento.style.fontSize = tamanhoNovo + "px";
-}
-
-function diminui(elemento) {
-  var tamanhoAtual = window.getComputedStyle(elemento).getPropertyValue("font-size");
-  var tamanhoNovo = parseFloat(tamanhoAtual) - 3;
-  elemento.style.fontSize = tamanhoNovo + "px";
-}
