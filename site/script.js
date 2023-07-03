f943b3e53ce6f2759eec2433c02f92a742674c87
Octávio Correia <octavio.correia2017@gmail.com>
2023-07-03 20:59:47 -0300

    Adicionando as frases de recomendações

--- site/script.js
+++ site/script.js
@@ -1,5 +1,19 @@
 window.addEventListener('DOMContentLoaded', function () {
   var texto = document.querySelector('.texto');
   texto.style.opacity = '1';
-});
 
+  var idx = 0;
+  var imgs = document.getElementById("containerCarrossel");
+  var img = document.querySelectorAll("#containerCarrossel img");
+
+  function frente() {
+    idx++;
+    if (idx > img.length - 1) {
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+  }
+
+  setInterval(frente, 5000);
+});
